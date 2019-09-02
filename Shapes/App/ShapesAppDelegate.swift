46370d9c187666d7d673db8c9b67cefe814c99df
Ralf Ebert <info@ralfebert.de>
2019-09-02 20:28:06 +0200

    Project setup Shapes

---
+++ Shapes/App/ShapesAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — Shapes
+
+import UIKit
+
+@UIApplicationMain
+class ShapesAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
